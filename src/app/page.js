c7bb5671359715da476a43d8cf73d6c183fa5ff7
Your Name <youremail@example.com>
2025-01-10 13:39:04 +0530

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,19 @@
-import Contact from "../components/contact";
+import { Contact } from "../components/contact";
 import About from "../components/About";
 import DirectorsDesk from "../components/DirectorsDesk";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 
-
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Hero/>
-      <About/>
-      <DirectorsDesk/>
-      <Contact/>
-      <Footer/>
+      <Hero />
+      <About />
+      <DirectorsDesk />
+      <Contact />
+      <Footer />
     </>
   );
 }
